83df189153008e91645040483b4be4ad3a11687c
brendancwood <brendan@brendancwood.com>
2014-06-26 16:43:24 -0700

    commit fixes to form and stats

--- js/form.js
+++ js/form.js
@@ -23,7 +23,7 @@ var day = currentTime.getDate()
 var year = currentTime.getFullYear()
 var date = month + "-" + day + "-" + year;
 //check to see if stats for current date exist, if so, populate vars with db data
-var checkRef = new Firebase('https://codehscore.firebaseio.com/users/' +userId+ '/stats/');
+var checkRef = new Firebase('https://codehscore.firebaseio.com/users/' +user.uid+ '/stats/');
 
 checkRef.on('value', function(snapshot) {
 	var data = snapshot.val();
@@ -70,7 +70,7 @@ $('.submit').on('click', function() {
 	var date = month + "-" + day + "-" + year;
 
 	// var dateRef = new Firebase('https://codehscore.firebaseio.com/users/simplelogin:28/stats/');
-	var checkRef = new Firebase('https://codehscore.firebaseio.com/users/simplelogin:28/stats/');
+	var checkRef = new Firebase('https://codehscore.firebaseio.com/users/' + user.uid + '/stats/');
 	
 	//check to see if stats for current date exist, if so, populate vars with db data
 	checkRef.on('value', function(snapshot) {
