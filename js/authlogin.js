76a3d6e8dc92e1ce85bd2f66cf48cb91e8caf884
zirbem07 <maxwell.zirbel@gmail.com>
2014-06-27 20:44:06 -0700

    updated nav

--- js/authlogin.js
+++ js/authlogin.js
@@ -5,23 +5,16 @@ $(document).ready(function(){
 	var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
 	  if (error) { 
 	    // an error occurred while attempting login
-          alert("Either your email or password is not correct");
 	    console.log(error);
 	  } else if (user) {
 	    // user authenticated with Firebase
 	    console.log('User ID: ' + user.uid + ', Provider: ' + user.provider);
-
-        window.open('newsfeed.html', '_self');
 	  } else {
 	    // user is logged out
 	  }
 	});
 
-    $('.userPassword').keydown(function(event) {
-        if (event.keyCode == 13) {
-            $(".login").click();
-        }
-    });
+
 
 
 	$('.login').on('click', function(){
